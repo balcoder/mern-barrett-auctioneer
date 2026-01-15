bcb4e84cdd420a155c1d166f8b2e6a1d14e4a403
balcoder <des.barrett@gmail.com>
2026-01-15 10:27:02 +0000

    added vite and tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-red-500 text-6xl">Welcome to the Auctioneer</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
